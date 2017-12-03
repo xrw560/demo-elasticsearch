a5dcf27b60c432b47967cd16d42574a75ba80a2a
zhouning <ncutits@163.com>
2017-12-03 19:15:18 +0800

    no message

--- src/main/java/com/sxt/es/EsController.java
+++ src/main/java/com/sxt/es/EsController.java
@@ -18,59 +18,60 @@ import java.util.Map;
 @Controller
 @RequestMapping("/")
 public class EsController {
-	static final Logger logger = LoggerFactory.getLogger(EsController.class);
-	HbaseUtils hbaseUtils = new HbaseUtils();
-	@Autowired
-	private Index index;
-	public static String path = "F:/index";
-	@RequestMapping("/create.do")
-	public String createIndex() throws Exception {
-		index.createIndex();
-		return "/create.jsp";
-	}
+    static final Logger logger = LoggerFactory.getLogger(EsController.class);
 
-	@RequestMapping("/search.do")
-	public String serachArticle(Model model,
-			@RequestParam(value="keyWords",required = false) String keyWords,
-			@RequestParam(value = "pageNum", defaultValue = "1") Integer pageNum,
-			@RequestParam(value = "pageSize", defaultValue = "3") Integer pageSize){
-		try {
-			keyWords = new String(keyWords.getBytes("ISO-8859-1"),"UTF-8");
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-		}
-		Map<String,Object> map = new HashMap<String, Object>();
-		int count = 0;
-		try {
-			map = Esutil.search(keyWords,"bjsxt","doc",(pageNum-1)*pageSize, pageSize);
-			count = Integer.parseInt(((Long) map.get("count")).toString());
-		} catch (Exception e) {
-			logger.error("查询索引错误!{}",e);
-			e.printStackTrace();
-		}
-		PageUtil<Map<String, Object>> page = new PageUtil<Map<String, Object>>(String.valueOf(pageNum),String.valueOf(pageSize),count);
-		List<Map<String, Object>> articleList = (List<Map<String, Object>>)map.get("dataList");
-		page.setList(articleList);
-		model.addAttribute("total",count);
-		model.addAttribute("pageNum",pageNum);
-		model.addAttribute("page",page);
-		model.addAttribute("kw",keyWords);
-		return "index.jsp";	
-	}
+    HbaseUtils hbaseUtils = new HbaseUtils();
+
+    @Autowired
+    private Index index;
+    public static String path = "F:/index";
+
+    @RequestMapping("/create.do")
+    public String createIndex() throws Exception {
+        index.createIndex();
+        return "/create.jsp";
+    }
+
+    @RequestMapping("/search.do")
+    public String serachArticle(Model model,
+                                @RequestParam(value = "keyWords", required = false) String keyWords,
+                                @RequestParam(value = "pageNum", defaultValue = "1") Integer pageNum,
+                                @RequestParam(value = "pageSize", defaultValue = "3") Integer pageSize) {
+        try {
+            keyWords = new String(keyWords.getBytes("ISO-8859-1"), "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        Map<String, Object> map = new HashMap<String, Object>();
+        int count = 0;
+        try {
+            map = Esutil.search(keyWords, "bjsxt", "doc", (pageNum - 1) * pageSize, pageSize);
+            count = Integer.parseInt(((Long) map.get("count")).toString());//总个数
+        } catch (Exception e) {
+            logger.error("查询索引错误!{}", e);
+            e.printStackTrace();
+        }
+        PageUtil<Map<String, Object>> page = new PageUtil<Map<String, Object>>(String.valueOf(pageNum), String.valueOf(pageSize), count);
+        List<Map<String, Object>> articleList = (List<Map<String, Object>>) map.get("dataList");
+        page.setList(articleList);
+        model.addAttribute("total", count);
+        model.addAttribute("pageNum", pageNum);
+        model.addAttribute("page", page);
+        model.addAttribute("kw", keyWords);
+        return "index.jsp";
+    }
 
 
-	/**
-	 * 查看文章详细信息
-	 * @return
-	 */
-	@RequestMapping("/detailDocById/{id}.do")
-	public String detailArticleById(@PathVariable(value="id") String id, Model modelMap) throws IOException {
-		//这里用的查询是直接从hbase中查询一条字符串出来做拆分封装，这里要求同学们用protobuffer
-		Doc Doc = hbaseUtils.get(hbaseUtils.TABLE_NAME, id);
-		modelMap.addAttribute("Doc",Doc);
-		return "/detail.jsp";
-	}
-	
-	
-	
+    /**
+     * 查看文章详细信息
+     */
+    @RequestMapping("/detailDocById/{id}.do")
+    public String detailArticleById(@PathVariable(value = "id") String id, Model modelMap) throws IOException {
+        //这里用的查询是直接从hbase中查询一条字符串出来做拆分封装，这里要求同学们用protobuffer
+        Doc Doc = hbaseUtils.get(hbaseUtils.TABLE_NAME, id);
+        modelMap.addAttribute("Doc", Doc);
+        return "/detail.jsp";
+    }
+
+
 }
--- src/main/java/com/sxt/es/Esutil.java
+++ src/main/java/com/sxt/es/Esutil.java
@@ -14,6 +14,7 @@ import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
 import org.elasticsearch.search.highlight.HighlightField;
+
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
@@ -22,95 +23,97 @@ import java.util.List;
 import java.util.Map;
 
 public class Esutil {
-	public static Client client = null;
+    public static Client client = null;
 
-		/**
-		 * 获取客户端
-		 * @return
-		 */
-		public static  Client getClient() {
-			if(client!=null){
-				return client;
-			}
-			Settings settings = Settings.settingsBuilder().put("cluster.name", "chenkl").build();
-			try {
-				client = TransportClient.builder().settings(settings).build()
-						.addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("master"), 9300))
-						.addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("slave1"), 9300))
-						.addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("slave2"), 9300));
-			} catch (UnknownHostException e) {
-				e.printStackTrace();
-			}
-			return client;
-		}
-	
-	
-	
-	
-	public static String addIndex(String index,String type,Doc Doc){
-		HashMap<String, Object> hashMap = new HashMap<String, Object>();
-		hashMap.put("id", Doc.getId());
-		hashMap.put("title", Doc.getTitle());
-		hashMap.put("describe", Doc.getDescribe());
-		hashMap.put("author", Doc.getAuthor());
-		
-		IndexResponse response = getClient().prepareIndex(index, type).setSource(hashMap).execute().actionGet();
-		return response.getId();
-	}
-	
-	
-	public static Map<String, Object> search(String key,String index,String type,int start,int row){
-		SearchRequestBuilder builder = getClient().prepareSearch(index);
-		builder.setTypes(type);
-		builder.setFrom(start);
-		builder.setSize(row);
-		//设置高亮字段名称
-		builder.addHighlightedField("title");
-		builder.addHighlightedField("describe");
-		//设置高亮前缀
-		builder.setHighlighterPreTags("<font color='red' >");
-		//设置高亮后缀
-		builder.setHighlighterPostTags("</font>");
-		builder.setSearchType(SearchType.DFS_QUERY_THEN_FETCH);
-		if(StringUtils.isNotBlank(key)){
+    /**
+     * 获取客户端
+     *
+     * @return
+     */
+    public static Client getClient() {
+        if (client != null) {
+            return client;
+        }
+        Settings settings = Settings.settingsBuilder().put("cluster.name", "chenkl").build();
+        try {
+            client = TransportClient.builder().settings(settings).build()
+                    .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("master"), 9300))
+                    .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("slave1"), 9300))
+                    .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("slave2"), 9300));
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return client;
+    }
+
+
+    public static String addIndex(String index, String type, Doc Doc) {
+        HashMap<String, Object> hashMap = new HashMap<String, Object>();
+        hashMap.put("id", Doc.getId());
+        hashMap.put("title", Doc.getTitle());
+        hashMap.put("describe", Doc.getDescribe());
+        hashMap.put("author", Doc.getAuthor());
+
+        IndexResponse response = getClient().prepareIndex(index, type).setSource(hashMap).execute().actionGet();
+        return response.getId();
+    }
+
+
+    public static Map<String, Object> search(String key, String index, String type, int start, int row) {
+        SearchRequestBuilder builder = getClient().prepareSearch(index);
+        builder.setTypes(type);
+        builder.setFrom(start);
+        builder.setSize(row);
+        //设置高亮字段名称
+        builder.addHighlightedField("title");
+        builder.addHighlightedField("describe");
+        //设置高亮前缀
+        builder.setHighlighterPreTags("<font color='red' >");
+        //设置高亮后缀
+        builder.setHighlighterPostTags("</font>");
+        builder.setSearchType(SearchType.DFS_QUERY_THEN_FETCH);
+        if (StringUtils.isNotBlank(key)) {
 //			builder.setQuery(QueryBuilders.termQuery("title",key));
-			builder.setQuery(QueryBuilders.multiMatchQuery(key, "title","describe"));
-		}
-		builder.setExplain(true);
-		SearchResponse searchResponse = builder.get();
-		
-		SearchHits hits = searchResponse.getHits();
-		long total = hits.getTotalHits();
-		Map<String, Object> map = new HashMap<String,Object>();
-		SearchHit[] hits2 = hits.getHits();
-		map.put("count", total);
-		List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
-		for (SearchHit searchHit : hits2) {
-			Map<String, HighlightField> highlightFields = searchHit.getHighlightFields();
-			HighlightField highlightField = highlightFields.get("title");
-			Map<String, Object> source = searchHit.getSource();
-			if(highlightField!=null){
-				Text[] fragments = highlightField.fragments();
-				String name = "";
-				for (Text text : fragments) {
-					name+=text;
-				}
-				source.put("title", name);
-			}
-			HighlightField highlightField2 = highlightFields.get("describe");
-			if(highlightField2!=null){
-				Text[] fragments = highlightField2.fragments();
-				String describe = "";
-				for (Text text : fragments) {
-					describe+=text;
-				}
-				source.put("describe", describe);
-			}
-			list.add(source);
-		}
-		map.put("dataList", list);
-		return map;
-	}
+            //查询条件匹配
+            builder.setQuery(QueryBuilders.multiMatchQuery(key, "title", "describe"));
+        }
+        builder.setExplain(true);
+        SearchResponse searchResponse = builder.get();
+
+        SearchHits hits = searchResponse.getHits();
+        //查所有总个数，分页用
+        long total = hits.getTotalHits();
+        Map<String, Object> map = new HashMap<String, Object>();
+        SearchHit[] hits2 = hits.getHits();
+        map.put("count", total);
+        //结果集合
+        List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+        for (SearchHit searchHit : hits2) {
+            Map<String, HighlightField> highlightFields = searchHit.getHighlightFields();
+            HighlightField highlightField = highlightFields.get("title");
+            Map<String, Object> source = searchHit.getSource();
+            if (highlightField != null) {
+                Text[] fragments = highlightField.fragments();
+                String name = "";
+                for (Text text : fragments) {
+                    name += text;
+                }
+                source.put("title", name);
+            }
+            HighlightField highlightField2 = highlightFields.get("describe");
+            if (highlightField2 != null) {
+                Text[] fragments = highlightField2.fragments();
+                String describe = "";
+                for (Text text : fragments) {
+                    describe += text;
+                }
+                source.put("describe", describe);
+            }
+            list.add(source);
+        }
+        map.put("dataList", list);
+        return map;
+    }
 
 //	public static void main(String[] args) {
 //		Map<String, Object> search = Esutil.search("hbase", "bjsxt", "doc", 0, 10);
